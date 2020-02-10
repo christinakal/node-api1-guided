aba1dafd872e9b60cfc6ab256c4d0509e7da78f5
Christina Kaliora <develop@pirron.one>
2020-02-10 19:28:47 +0100

    api is up and running

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Hubs = require('./data/hubs-model.js');
+const server = express();
+
+server.get('/', (req, res) => {
+    res.json({hello: 'world'})
+})
+
+const port = 5000;
+
+server.get('/api/hubs', (req, res) => {
+    Hubs.find().then(hubs => {
+        res.status(200).json(hubs);
+    }).catch( err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: 'oops'})
+    });
+})
+
+server.listen(port, () => {
+    console.log(`API on port ${port}`);
+})
